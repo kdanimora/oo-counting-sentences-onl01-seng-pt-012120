a632fc9db5a14db0381054b6d98d63b5d0080182
Karen Daniela <kdaniimora@gmail.com>
2020-02-10 19:04:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -31,6 +31,15 @@ end
   end
 
   def count_sentences
+    nos = 0 
+
+
+   self.split(' ').each do |word| 
+    if word[-1] == "." || word[-1] == "!" || word[-1] == "?"
+      nos += 1
+    end 
+  end 
+  p nos
 
   end
 end
