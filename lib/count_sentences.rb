8ff0bb8d346a83a22cc22df91c8eec3f140d33ab
Karen Daniela <kdaniimora@gmail.com>
2020-02-10 18:55:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] != "."
+      false
+    else
+      true 
+    end
   end
+
+
 
   def question?
-
+if self[-1] != "?"
+  false 
+else 
+  true 
   end
+end 
+
 
   def exclamation?
+    if self[-1] != "!"
+      false
+    else
+      true 
+    end 
 
   end
 
